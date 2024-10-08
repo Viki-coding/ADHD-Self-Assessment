81b39437e06e7310a480b039f8ae9d5453c5a85d
Viki-coding <vikimulhall@gmail.com>
2024-10-08 14:48:11 +0000

    Add ratings for each question

--- run.py
+++ run.py
@@ -18,16 +18,18 @@ else:
 """
 Insert assessment questions
 """
-questions = ("1. How often do you have trouble wrapping up the final details of a project, once the challenging parts have been done?", 
-"2. How often do you have difficulty getting things in order when you have to do a task that requires organization?", 
-"3. How often do you have problems remembering appointments or obligations?"
-"4. When you have a task that requires a lot of thought, how often do you avoid or delay getting started?",
-"5. How often do you fidget or squirm with your hands or feet when you have to sit down for a long time?", 
-"6. How often do you feel overly active and compelled to do things, like you were driven by a motor?")
+questions = ["How often do you have trouble wrapping up the final details of a project, once the challenging parts have been done?", "How often do you have difficulty getting things in order when you have to do a task that requires organization?", 
+"How often do you have problems remembering appointments or obligations?", "When you have a task that requires a lot of thought, how often do you avoid or delay getting started?", "How often do you fidget or squirm with your hands or feet when you have to sit down for a long time?", "How often do you feel overly active and compelled to do things, like you were driven by a motor?" ]
 
 ratings = (("A. Never", "B. Rarely", "C. Sometimes", "D. Often", "Very Often"), 
-("A. Never", "B. Rarely", "C. Sometimes", "D. Often", "Very Often"), 
-("A. Never", "B. Rarely", "C. Sometimes", "D. Often", "Very Often"), 
-("A. Never", "B. Rarely", "C. Sometimes", "D. Often", "Very Often"), 
-("A. Never", "B. Rarely", "C. Sometimes", "D. Often", "Very Often"), 
-("A. Never", "B. Rarely", "C. Sometimes", "D. Often", "Very Often"))
+            ("A. Never", "B. Rarely", "C. Sometimes", "D. Often", "E. Very Often"), 
+            ("A. Never", "B. Rarely", "C. Sometimes", "D. Often", "E. Very Often"), 
+            ("A. Never", "B. Rarely", "C. Sometimes", "D. Often", "E. Very Often"), 
+            ("A. Never", "B. Rarely", "C. Sometimes", "D. Often", "E. Very Often"), 
+            ("A. Never", "B. Rarely", "C. Sometimes", "D. Often", "E. Very Often"))
+
+question_num = 0 
+
+for question in questions:
+    print ("================================")
+    input(questions)
