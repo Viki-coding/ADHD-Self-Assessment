db5c11f84c6f431defd615feb2a8449aa2f5e823
Viki-coding <vikimulhall@gmail.com>
2024-10-08 18:54:55 +0000

    Comment out actual questions and insert 1 liners to visually see if code diplays when tested. -m Create function to iterate through questions.

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 """
 Get user input for name and age
+Error Handling Name - if they put nothing - alert - if they put numbers - alert
+Error Handing Age - if they put nothering - alert - if they put string - alert
 """
 name = input ("What is your name?  ")
 
@@ -10,7 +12,7 @@ if name == "":
 age = int(input("Enter your age: "))
 
 if age >= 17:
-    print(f"Hello {name} Welcome to the ADHD self assessment tool \n")
+    print(f"Hello {name} welcome to the ADHD self assessment tool \n")
 else:
     print(f"Sorry {name}. You must be 17 years or older for this test \n")
 
@@ -18,18 +20,36 @@ else:
 """
 Insert assessment questions
 """
-questions = ["How often do you have trouble wrapping up the final details of a project, once the challenging parts have been done?", "How often do you have difficulty getting things in order when you have to do a task that requires organization?", 
-"How often do you have problems remembering appointments or obligations?", "When you have a task that requires a lot of thought, how often do you avoid or delay getting started?", "How often do you fidget or squirm with your hands or feet when you have to sit down for a long time?", "How often do you feel overly active and compelled to do things, like you were driven by a motor?" ]
+questions = ("Question1: ", "Question2: ", "Question3:")
 
-ratings = (("A. Never", "B. Rarely", "C. Sometimes", "D. Often", "Very Often"), 
-            ("A. Never", "B. Rarely", "C. Sometimes", "D. Often", "E. Very Often"), 
-            ("A. Never", "B. Rarely", "C. Sometimes", "D. Often", "E. Very Often"), 
-            ("A. Never", "B. Rarely", "C. Sometimes", "D. Often", "E. Very Often"), 
-            ("A. Never", "B. Rarely", "C. Sometimes", "D. Often", "E. Very Often"), 
-            ("A. Never", "B. Rarely", "C. Sometimes", "D. Often", "E. Very Often"))
+# questions = ("How often do you have trouble wrapping up the final details of a project, once the challenging parts have been done?", "How often do you have difficulty getting things in order when you have to do a task that requires organization?", 
+# "How often do you have problems remembering appointments or obligations?", "When you have a task that requires a lot of thought, how often do you avoid or delay getting started?", "How often do you fidget or squirm with your hands or feet when you have to sit down for a long time?", "How often do you feel overly active and compelled to do things, like you were driven by a motor?" )
 
-question_num = 0 
+ratings = ("A. Never", "B. Rarely", "C. Sometimes", "D. Often", "E. Very Often")
+
+# answers = []
+# questions_num = 0 
+# points = 0 
 
 for question in questions:
-    print ("================================")
-    input(questions)
+    print("-------------------------------")
+    print(question, flush=True)
+    for rating in ratings[questions_num]:
+        print(rating)
+
+"""
+for question in questions:
+    response = input(question + " ")
+    if response.lower() == "A" or response == "B":
+        points = points + 1
+
+if points == 0:
+    print (f"{name} you do not have any sympthoms that are consistent with ADHD")
+if points == >=6:
+    print (f"{name} you have some sympthoms that are consistent with ADHD we recommend you look into this further")
+if points == >=12:
+    print (f"{name} you have sympthoms that are consistent with ADHD we recommend you look into this further")
+if point == >= 15:
+    print (f"{name} you sympthoms that are consistent with ADHD we recommend you look into this further")
+
+"""
