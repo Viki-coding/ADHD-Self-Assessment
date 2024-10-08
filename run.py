851cf35c8c3c2b8bf20fc1bc932e59bccc5a273c
Viki-coding <vikimulhall@gmail.com>
2024-10-08 13:43:24 +0000

    Enter user inputs and check with if statement

--- run.py
+++ run.py
@@ -2,10 +2,15 @@
 Get user input for name and age
 """
 name = input ("What is your name?  ")
+
+if name == "":
+    print("You did not enter your name!")
+    name = input ("What is your name?  ")
+
 age = int(input("Enter your age: "))
 
 if age >= 17:
-    print(f"Hello" + {name} "Welcome to the ADHD self assessment tool \n")
+    print(f"Hello {name} Welcome to the ADHD self assessment tool \n")
 else:
-    print(f"Sorry" + {name} "You must be 17 years or older for this test \n")
+    print(f"Sorry {name}. You must be 17 years or older for this test \n")
 
