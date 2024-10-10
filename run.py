4b78b104be05645c593994d80a85b17d6c6957d0
Viki-coding <vikimulhall@gmail.com>
2024-10-10 13:53:31 +0000

    Insert : at end of get_name function

--- run.py
+++ run.py
@@ -14,6 +14,7 @@ def get_age():
             print("INVALID! Please enter your age")
             print(error)
             age = int(input("Enter your age:  \n"))
+    return age
         
 get_age()
 
@@ -29,7 +30,7 @@ def run():
    
 run() 
 
-def get_name()
+def get_name():
     """
     Get users name 
     Validate that it is a string and not numbers or no value
@@ -42,7 +43,7 @@ def get_name()
     else:
         print(f"Hello {name} welcome to the the ADHD assessment")
 
- get_name()
+get_name()
 
     # ask question
 
