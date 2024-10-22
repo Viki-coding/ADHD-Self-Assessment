9747f62e7132e0caf560dd9040d204078a1fc741
Viki Mulhall <vikimulhall@gmail.com>
2024-10-22 09:18:36 +0000

    Delete trailing white space after code

--- run.py
+++ run.py
@@ -7,14 +7,14 @@ def get_age():
     while True:
         age = input("Enter your age and press return:  \n")
         try:
-            age = int(age) 
-            return age 
+            age = int(age)
+            return age
         except ValueError:
             print("INVALID! Please enter a valid age")
 
 def get_name():
     """
-    Get users name 
+    Get users name
     Validate that it is a string and not numbers or no value
     :return: string representing users name
     """
@@ -23,19 +23,19 @@ def get_name():
         name = input("What is your name?  \n")
         if name == "":
             print("You did not enter your name!")
-        elif not name.isalpha(): 
+        elif not name.isalpha():
             print("Name should only contain letters")
         else:
             print(f"Hello {name} welcome to the ADHD assessment")
-            return name           
+            return name
 
 
 def run():
     """
-    User must be over 17 and under 110 to do test 
+    User must be over 17 and under 110 to do test
     """
     while True:
-        age = get_age()    
+        age = get_age()
         if age < 17:
             print("Sorry you must be 17 to do this test! \n")
         elif age > 110:
@@ -53,11 +53,11 @@ def ask_questions(name):
     Insert dictionary of assessment questions with options
     """
     questions = [
-        " QUESTION 1: \n \n How often do you have trouble wrapping up the final details of a project, \n once the challenging parts have been done?\n", 
-        " QUESTION 2: \n \n How often do you have difficulty getting things in order \n when you have to do a task that requires organization?\n", 
-        " QUESTION 3: \n \n How often do you have problems remembering appointments or obligations?\n", 
-        " QUESTION 4: \n \n When you have a task that requires a lot of thought, \n how often do you avoid or delay getting started?\n", 
-        " QUESTION 5: \n \n How often do you fidget or squirm with your hands or feet \n when you have to sit down for a long time?\n", 
+        " QUESTION 1: \n \n How often do you have trouble wrapping up the final details of a project, \n once the challenging parts have been done?\n",
+        " QUESTION 2: \n \n How often do you have difficulty getting things in order \n when you have to do a task that requires organization?\n",
+        " QUESTION 3: \n \n How often do you have problems remembering appointments or obligations?\n",
+        " QUESTION 4: \n \n When you have a task that requires a lot of thought, \n how often do you avoid or delay getting started?\n",
+        " QUESTION 5: \n \n How often do you fidget or squirm with your hands or feet \n when you have to sit down for a long time?\n",
         " QUESTION 6: \n \n How often do you feel overly active and compelled to do things,\n like you were driven by a motor?\n"
     ]
 
@@ -83,7 +83,7 @@ def ask_questions(name):
         print("------------------------------------------------------------------------")
         for option in options:
             print(option)
-        while True: 
+        while True:
             response = input("\n Enter A, B, C, D, or E:  ").lower()
             if response in scoring:
                 total_score += scoring[response]
@@ -99,13 +99,13 @@ def ask_questions(name):
     print(" ---------------------------------------------------------------------\n")
 
     if 0 <= total_score <= 3:
-        print (f" {name} you scored {total_score}. You do NOT have any symptoms that are consistent with ADHD")
+        print(f" {name} you scored {total_score}. You do NOT have any symptoms that are consistent with ADHD")
     elif 4 <= total_score <= 6:
-        print (f" {name} you scored {total_score}. You have some minor symptoms that are indicative \n with ADHD we recommend you look into this further.")
+        print(f" {name} you scored {total_score}. You have some minor symptoms that are indicative \n with ADHD we recommend you look into this further.")
     elif 7 <= total_score <= 12:
-        print (f" {name} you scored {total_score}. You have symptoms that are indicative \n with ADHD we recommend you look into this further.")
+        print(f" {name} you scored {total_score}. You have symptoms that are indicative \n with ADHD we recommend you look into this further.")
     elif 13 <= total_score <= 18:
-        print (f" {name} you scored {total_score}. Your high score shows indicative symptoms consistent with ADHD in adults \n we highly recommend you look into this further.")
+        print(f" {name} you scored {total_score}. Your high score shows indicative symptoms consistent with ADHD in adults \n we highly recommend you look into this further.")
 
 print("---------------------------------------------------------------------")
 print("                  Welcome to the ADHD assessment tool              \n")
@@ -115,8 +115,4 @@ print(" This program was developed as a coding educational challenge \n and is N
 print("---------------------------------------------------------------------\n")
 
 if __name__ == '__main__':
-    run() 
-    
-       
-  
-
+    run()
