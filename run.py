b4c2f9ff5fc455e8968a68ffc0db9d0720f57974
Viki Mulhall <vikimulhall@gmail.com>
2024-10-22 09:30:56 +0000

    Delete white lines and spaces after running code through CI python linter. Insert spaces after ()

--- run.py
+++ run.py
@@ -12,13 +12,13 @@ def get_age():
         except ValueError:
             print("INVALID! Please enter a valid age")
 
+
 def get_name():
     """
     Get users name
     Validate that it is a string and not numbers or no value
     :return: string representing users name
     """
-    
     while True:
         name = input("What is your name?  \n")
         if name == "":
@@ -44,10 +44,13 @@ def run():
             name = get_name()
             ask_questions(name)
             break
-   
+
+
 """
 Create a ask_questions function, using the name as a parameter so that we can use the users name value when we are calling the function later displaying their results
 """
+
+
 def ask_questions(name):
     """
     Insert dictionary of assessment questions with options
@@ -65,12 +68,11 @@ def ask_questions(name):
     """
     Apply an integer score against each possible user choice
     """
-    scoring = {"a": 0, "b":0, "c": 1, "d":2, "e":3}
+    scoring = {"a": 0, "b" : 0, "c" : 1, "d" : 2, "e" : 3}
     """
     Initialise the total score
     """
     total_score = 0
-   
     """
     Iterate through the questions and display the rating the user chooses.
     Ask user for the input a, b, c, d, or e.  Apply .lower() to input encase user puts in a lowercase letter
@@ -90,7 +92,6 @@ def ask_questions(name):
                 break
             else:
                 print("Invalid response.  Please enter A, B, C, D or E")
-    
     """
     Display if user has symptoms that are consistent with ADHD depending on the score range
     """
@@ -106,6 +107,7 @@ def ask_questions(name):
         print(f" {name} you scored {total_score}. You have symptoms that are indicative \n with ADHD we recommend you look into this further.")
     elif 13 <= total_score <= 18:
         print(f" {name} you scored {total_score}. Your high score shows indicative symptoms consistent with ADHD in adults \n we highly recommend you look into this further.")
+
 
 print("---------------------------------------------------------------------")
 print("                  Welcome to the ADHD assessment tool              \n")
